74a3cb3f23af75f7eabfb1b5e96480b047db736f
yousefhamarsheh <yousef747hamarsheh@gmail.com>
2022-02-20 21:51:45 +0200

    password function

--- controllers/ForgetController/post.php
+++ controllers/ForgetController/post.php
@@ -2,15 +2,14 @@
 
     include_once '../../config/Database.php';
 
-    $customer_id = $_POST['id'];
-    $customer_email = $_POST['email'];
+    $customer_username = $_POST['username'];
     $customer_password = $_POST['password'];
 
     $pdo = new PDO('mysql:host=localhost;dbname=hoteldb','root12345','root12345');
 
 
 
-    $sql = "UPDATE customer SET customer_password = '{$customer_password}' WHERE customer_id = {$customer_id}";
+    $sql = "UPDATE customer SET customer_password = '{$customer_password}' WHERE customer_username = '{$customer_username}'";
     $stmt= $pdo->prepare($sql);
 
     if($stmt->execute()){
